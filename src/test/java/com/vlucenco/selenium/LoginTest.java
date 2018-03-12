f94bc3d370d9f142e4b586e912a13ac917435928
Vitali Lucenco <lucenco.v@gmail.com>
2018-03-12 10:51:40 +0200

    Create loginToAdminPanel test

--- src/test/java/com/vlucenco/selenium/LoginTest.java
+++ src/test/java/com/vlucenco/selenium/LoginTest.java
@@ -1,6 +1,7 @@
 package com.vlucenco.selenium;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyFirstTest {
+public class LoginTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
@@ -21,10 +22,13 @@ public class MyFirstTest {
     }
 
     @Test
-    public void myFirstTest() throws InterruptedException {
-        driver.get("http://google.com/");
-        driver.findElement(By.name("q")).sendKeys("webdriver\n");
-        wait.until(ExpectedConditions.titleContains("webdriver - "));
+    public void loginToAdminPanel(){
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(ExpectedConditions.titleContains("My Store"));
+        Assert.assertTrue(driver.findElement(By.className("fa-sign-out")).isDisplayed());
     }
 
     @After
