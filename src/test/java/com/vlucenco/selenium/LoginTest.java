8af0ca656b80e0f39bcee08f0b9348c2b43c3713
Vitali Lucenco <lucenco.v@gmail.com>
2018-03-13 09:57:07 +0200

    Refactor login test

--- src/test/java/com/vlucenco/selenium/LoginTest.java
+++ src/test/java/com/vlucenco/selenium/LoginTest.java
@@ -1,7 +1,6 @@
 package com.vlucenco.selenium;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -22,13 +21,12 @@ public class LoginTest {
     }
 
     @Test
-    public void loginToAdminPanel(){
+    public void loginToAdminPanel() {
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        wait.until(ExpectedConditions.titleContains("My Store"));
-        Assert.assertTrue(driver.findElement(By.className("fa-sign-out")).isDisplayed());
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("fa-sign-out")));
     }
 
     @After
