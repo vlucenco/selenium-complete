147f141f2d22e6874aa73ef3d5f78cfd70855d48
Vitali Lucenco <lucenco.v@gmail.com>
2018-03-24 15:21:13 +0200

    Refactor previous test to verify sorting of countries and their zones if any

--- src/test/java/com/vlucenco/selenium/AdminPanelTest.java
+++ src/test/java/com/vlucenco/selenium/AdminPanelTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,6 +15,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AdminPanelTest extends TestBase {
+
+    private static final String COUNTRIES_PAGE_URL = "http://localhost/litecart/admin/?app=countries&doc=countries";
 
     private By menuItemLocator = By.id("app-");
     private By subMenuItemLocator = By.cssSelector("#app-.selected li");
@@ -37,29 +40,58 @@ public class AdminPanelTest extends TestBase {
         }
     }
 
-    @Test
-    public void testCountriesAlphabeticalSorting() {
-        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-        List<WebElement> elements = findElements(By.xpath("//*[@class='row']/td[5]"));
-
-        List<String> actuallySortedCountries = new ArrayList<>();
-        for (WebElement element : elements) {
-            actuallySortedCountries.add(element.getText());
-        }
-
-        checkCollectionSorted(actuallySortedCountries);
-    }
-
-    private void checkCollectionSorted(List<String> actuallySortedCountries) {
-        List<String> expectedAlphabeticallySortedCountries = new ArrayList<>(actuallySortedCountries)
-                .stream().sorted().collect(Collectors.toList());
-        Assert.assertEquals("Countries sorting not alphabetical", expectedAlphabeticallySortedCountries, actuallySortedCountries);
-    }
-
     private void checkPageHeader(int index, By locator) {
         List<WebElement> elements = findElements(locator);
         elements.get(index).click();
         Assert.assertTrue(findElement(By.tagName("h1")).isDisplayed());
+    }
+
+    @Test
+    public void testCountriesAndTheirZonesAreSortedAlphabetically() {
+        List<WebElement> countryRows = getCountryRows();
+        List<String> actuallySortedCountries = new ArrayList<>();
+
+        for (int row = 0; row < countryRows.size(); row++) {
+            if (countryHasZones(countryRows.get(row))) {
+                verifyCountryZonesSorting(countryRows, row);
+
+                countryRows = getCountryRows();
+            } else {
+                String countryName = countryRows.get(row).findElement(By.xpath("./td[5]")).getText();
+                actuallySortedCountries.add(countryName);
+            }
+        }
+
+        verifyCollectionSorted(actuallySortedCountries);
+    }
+
+    private List<WebElement> getCountryRows() {
+        driver.get(COUNTRIES_PAGE_URL);
+        return findElements(By.xpath("//*[@class='row']"));
+    }
+
+    private boolean countryHasZones(WebElement element) {
+        try {
+            int zonesNum = Integer.parseInt(element.findElement(By.xpath("./td[6]")).getText());
+            return zonesNum > 0;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private void verifyCountryZonesSorting(List<WebElement> countryRows, int row) {
+        countryRows.get(row).findElement(By.tagName("a")).click();
+        List<WebElement> zones = findElements(By.xpath("//td[3]/input[@type='hidden']/.."));
+        List<String> actuallySortedZones = new ArrayList<>();
+        zones.forEach(zone -> actuallySortedZones.add(zone.getText()));
+        verifyCollectionSorted(actuallySortedZones);
+    }
+
+    private void verifyCollectionSorted(List<String> actuallySortedElements) {
+        List<String> expectedAlphabeticallySortedElements = new ArrayList<>(actuallySortedElements)
+                .stream().sorted().collect(Collectors.toList());
+        Assert.assertEquals("Elements sorting not alphabetical",
+                expectedAlphabeticallySortedElements, actuallySortedElements);
     }
 
     @After
