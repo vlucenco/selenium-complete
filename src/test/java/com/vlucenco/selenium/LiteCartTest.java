f0b87873b19db8d9001026359d14131d2e7e4fee
Vitali Lucenco <lucenco.v@gmail.com>
2018-03-29 17:40:20 +0300

    Create test that verifies selecting a Product on Main Page leads to corresponding Product page

--- src/test/java/com/vlucenco/selenium/LiteCartTest.java
+++ src/test/java/com/vlucenco/selenium/LiteCartTest.java
@@ -13,6 +13,9 @@ import java.util.List;
 
 public class LiteCartTest extends TestBase {
 
+    private static final By regularPrice = By.cssSelector("s[class=regular-price]");
+    private static final By campaignPrice = By.cssSelector("strong[class=campaign-price]");
+
     @Before
     public void start() {
         driver = new ChromeDriver();
@@ -27,6 +30,62 @@ public class LiteCartTest extends TestBase {
             Assert.assertTrue("Number of stickers for the product not equal to 1",
                     product.findElements(By.className("sticker")).size() == 1);
         }
+    }
+
+    @Test
+    public void testSelectProductOnMainPageLeadsToCorrectProductPage() {
+        // Verifications on Main Page
+        WebElement product = findElement(By.cssSelector("#box-campaigns li"));
+
+        verifyProductRegularPriceIsGrey(product);
+        verifyProductCampaignPriceIsRed(product);
+
+        Double mainPageRegularPriceSize = getElementFontSize(product.findElement(regularPrice));
+        Double mainPageCampaignPriceSize = getElementFontSize(product.findElement(campaignPrice));
+        Assert.assertTrue("Campaign price should be bigger than regular price", mainPageRegularPriceSize < mainPageCampaignPriceSize);
+
+        String mainPageProductName = product.findElement(By.className("name")).getText();
+        String mainPageRegularPrice = product.findElement(regularPrice).getText();
+        String mainPageCampaignPrice = product.findElement(campaignPrice).getText();
+
+        product.click();
+
+        //Verifications on Product page
+        verifyProductRegularPriceIsGrey(findElement(By.id("box-product")));
+        verifyProductCampaignPriceIsRed(findElement(By.id("box-product")));
+
+        Double productPageRegularPriceSize = getElementFontSize(findElement(regularPrice));
+        Double productPageCampaignPriceSize = getElementFontSize(findElement(campaignPrice));
+        Assert.assertTrue("Campaign price should be bigger than regular price", productPageRegularPriceSize < productPageCampaignPriceSize);
+
+
+        String productPageName = findElement(By.tagName("h1")).getText();
+        String productPageRegularPrice = findElement(regularPrice).getText();
+        String productPageCampaignPrice = findElement(campaignPrice).getText();
+
+        Assert.assertEquals(mainPageProductName, productPageName);
+        Assert.assertEquals(mainPageRegularPrice, productPageRegularPrice);
+        Assert.assertEquals(mainPageCampaignPrice, productPageCampaignPrice);
+    }
+
+    private Double getElementFontSize(WebElement product) {
+        String sizeStr = product.getCssValue("font-size");
+        return Double.valueOf(sizeStr.replaceAll("px", ""));
+    }
+
+    private void verifyProductRegularPriceIsGrey(WebElement product) {
+        String[] regularPriceRGB = getElementRGBValues(product.findElement(regularPrice));
+        Assert.assertTrue(regularPriceRGB[0].equals(regularPriceRGB[2]) && regularPriceRGB[1].equals(regularPriceRGB[2]));
+    }
+
+    private void verifyProductCampaignPriceIsRed(WebElement product) {
+        String[] campaignPriceRGB = getElementRGBValues(product.findElement(campaignPrice));
+        Assert.assertTrue(!campaignPriceRGB[0].equals(campaignPriceRGB[2]) && campaignPriceRGB[1].equals(campaignPriceRGB[2]));
+    }
+
+    private String[] getElementRGBValues(WebElement element) {
+        String colorStyle = element.getCssValue("color");
+        return colorStyle.replaceAll("\\D", " ").trim().split("\\s+");
     }
 
     @After
